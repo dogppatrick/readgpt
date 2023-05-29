0a46ca1cd16755880f75d57eaa1cb29af9779a27
patrick <patrick@stockfeel.com.tw>
2023-05-29 12:20:22 +0800

    [feat] add js and cache to api

--- router.py
+++ router.py
@@ -5,24 +5,26 @@ from summary import summarize_article_chatgpt
 
 gpt_summary = APIRouter(prefix='/gpt_summary')
 
+cache = dict()
+
 @gpt_summary.post("/")
 async def process_url(request: dict):
     payload = request.get("data", {})
     target_url = payload.get('url')
+    if cache.get(target_url):
+        return cache.get(target_url)
+    else:
+        print(f'no cache exists')
     processed_data = f"Processing URL: {target_url}"
     content_text = web_context_extract(target_url)
+    content_text = content_text[:500]
     summary = summarize_article_chatgpt(content_text)
- 
-    return {"result": processed_data, "summary":summary}
+    response = {"result": processed_data, "summary":summary}
+    cache[target_url] = response
+    return response
 
 def web_context_extract(url):
     resp = requests.get(url)
     resp_text = resp.text
     content_text = sf_article_extract(resp_text)
     return content_text
-
-# url = 'https://www.stockfeel.com.tw/%e8%b2%a1%e7%b6%93%e6%96%b0%e8%81%9e-%e6%96%b0%e8%81%9e%e4%ba%8b%e4%bb%b6-%e5%9c%8b%e9%9a%9b%e5%a4%a7%e4%ba%8b/'
-# target_url = url
-# processed_data = f"Processing URL: {target_url}"
-# content_text = web_context_extract(target_url)
-# summary = summarize_article_chatgpt(content_text)
--- tmp.js
+++ tmp.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name         Add Current URL to Payload
+// @name         current url to gpt summary
 // @namespace    http://your-namespace.com
-// @version      1.0
+// @version      1.1
 // @description  Adds the current URL to the payload when making requests.
 // @match        https://www.ptt.cc/bbs/*
+// @match        https://www.stockfeel.com.tw/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,8 +15,8 @@
     const button = document.createElement('button');
     button.textContent = 'Send Request';
     button.style.position = 'fixed';
-    button.style.top = '50px';
-    button.style.left = '50px';
+    button.style.top = '250px';
+    button.style.right = '50px';
     document.body.appendChild(button);
 
     // 點擊按鈕時執行請求
@@ -32,16 +33,20 @@
             },
             body: JSON.stringify({ data: { url: currentUrl } }) // 將當前網址作為 payload
         })
-            .then(response => response.text()) // 將回應轉換為文字
+            .then(response => response.json()) // 將回應轉換為文字
             .then(data => {
+                const summary = data.summary;
                 // 顯示回應在畫面上
                 const responseDiv = document.createElement('div');
-                responseDiv.textContent = data;
+                responseDiv.textContent = summary;
                 responseDiv.style.position = 'fixed';
-                responseDiv.style.top = '80px';
-                responseDiv.style.left = '50px';
+                responseDiv.style.top = '350px';
+                responseDiv.style.right = '50px';
                 responseDiv.style.backgroundColor = 'white';
                 responseDiv.style.padding = '10px';
+                responseDiv.style.border = '2px solid black';
+                responseDiv.style.width = '500px';
+                responseDiv.style.whiteSpace = 'pre-wrap';
                 document.body.appendChild(responseDiv);
             })
             .catch(error => {
